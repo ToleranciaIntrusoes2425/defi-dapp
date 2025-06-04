3593e7d1f2e833e1c6d40dae772b816a598d1045
tiagosantos996@hotmail.com <tiagosantos996@hotmail.com>
2025-06-04 15:39:52 +0100

    Timestamp-upd

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
-const defiContractAddress = '0x5d49de47AB4FB5805aACacfe6BCf041309401789';
-const nftContractAddress = '0xA867C170c8Fe3340652232F388Ffc14273b6ADa8';
+const defiContractAddress = '0x2951bF77D78b9f70DCF9763832E710aCcce69a37';
+const nftContractAddress = '0x970AE69A58C93096C20E089d89949F3B37d07994';
 const nullAddress = '0x0000000000000000000000000000000000000000';
 
 export {
